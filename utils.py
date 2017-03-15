50e78da524418e3016fddbd127d35dcd4bbac3de
saurav <dhitals@me.com>
2017-03-15 18:11:01 -0400

    summary plots done

--- utils.py
+++ utils.py
@@ -1,42 +1,39 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import seaborn as sns
 import pandas as pd
-import pint
 
 from app import app, engine, Session
 from app.models import User, Activity, Streams
 
 def m_to_mile(x):
+    """ convert distance from meter to miles """
     return x * 0.000621371 # meter --> mile
 
-def ns_to_hr(x):
-    """ """
-    return x * 1e-9 * 0.000277778 * 1e-9 # ns --> s --> hr
+def ns_to_hms(x, sexagesimal=False):
+    """ Convert time units: nanosecond to hour """
+    hh = x * 1e-9 * 0.000277778 # ns --> s --> hr
 
-def speed_to_pace(speed):
-    """ Convert speed (m/s) to pace (MM:SS/mile)"""
-    
-    u = pint.UnitRegistry()
-    
-    pace = 1 / (speed * u.meter / u.second).to(u.mile / u.minute)
-    pace = ':'.join([str(int(pace.magnitude)),                 
-                     str(int((pace.magnitude * 60) % 60)).zfill(2)])
+    if sexagesimal is True:
+        hms = [str(int(hh)), 
+                str(int(hh * 60) % 60).zfill(2),
+                str(int(hh * 2660) % 60).zfill(2)]
+
+        hh = ':'.join(hms) if t.magnitude >= 1 else ':'.join(hms[1:])
+
+    return hh        
+
+def speed_to_pace(x, sexagesimal=False):
+    """ convert speed in m/s to pace in min/mile 
+        optionally, return sexagesimal pace
+    """
+    pace = 26.8224 / x # 1 m/s --> min/mile
+
+    if sexagesimal is True:
+        pace = ':'.join([str(int(pace.magnitude)),                 
+                             str(int((pace.magnitude * 60) % 60)).zfill(2)])
+
     return pace
-
-def nanosecond_to_hms(t):
-    """ Convert time in nanosecond to HH:MM:SS """
-    u = pint.UnitRegistry()
-    
-    # convert from nanoseconds to hour
-    t = (t * 1e-9 * u.second).to(u.hour)
-    
-    hms = [str(int(t.magnitude)),
-           str(int(t.magnitude * 60) % 60).zfill(2),
-           str(int((t.magnitude * 3600) % 60)).zfill(2)]
-    
-    t = ':'.join(hms) if t.magnitude >= 1 else ':'.join(hms[1:])
-                    
-    return t
 
 
 class summarize(object):
@@ -65,7 +62,7 @@ class summarize(object):
         # stats fields to summarize
         if fields is None:
             fields = [  'distance', 'moving_time',
-                        'average_speed', 'max_speed',
+                        'average_speed',
                         'average_heartrate', 'max_heartrate',
                         'average_cadence', 'max_cadence',
                         'suffer_score', 'total_elevation_gain' ]
@@ -91,7 +88,7 @@ class summarize(object):
         # remove some agg columns that don't make sense. e.g., `sum` of `average_cadence`
         df.drop([ 'average_cadence_sum', 
                   'average_heartrate_sum', 'max_heartrate_sum',
-                  'average_speed_sum', 'max_speed_sum',
+                  'average_speed_sum', #'max_speed_sum',
                   'suffer_score_sum', 'total_elevation_gain_mean'], axis=1, inplace=True)
 
         self.summary_df = df.sort_index(ascending=False)
@@ -106,57 +103,85 @@ class summarize(object):
 
         for col in df.columns:
             if 'distance' in col:
-                df[col] = df[col].apply(lambda x: (x * u.meter).to(u.mile).magnitude).round(2)
+                df[col] = df[col].apply(m_to_mile).round(2)
             elif 'time' in col:
-                df[col] = df[col].apply(nanosecond_to_hms)
+                df[col] = df[col].apply(ns_to_hms)
             elif 'speed' in col:
                 df[col] = df[col].apply(speed_to_pace)
-            elif 'elevation' in col:
-                df[col] = df[col].apply(lambda x: (x * u.meter).to(u.feet).magnitude).round(0)
             else:
                 df[col] = df[col].round()
 
         return df
 
+
+    def plot_fill(self, x, ymax, ymean, ax=None, label=None, fill=True):
+    
+        colors = ('r', 'b')
+        stat = ('max', 'total') if 'elevation' in label else ('max', 'avg')
+
+        for y, color, stat in zip((ymax, ymean), colors, stat):
+            ax.plot(x, y, color=color, marker='o', 
+                    linestyle=None, alpha=0.9, label='%s %s' %(stat, label))
+            if fill is True:
+                ax.fill_between(x, y, interpolate=True, color=color, alpha=0.5)
+        return
+
     def plot(self):
 
         df = self.summary_df.copy()
 
-        fig, ax = plt.subplots(3,2, figsize=(16,10))
+        fig, axes = plt.subplots(4,2, figsize=(16,16))
+        plt.tight_layout()
 
-        fig, axes = plt.subplots(4,2, figsize=(16,16), sharex=True)
+        # convert `periodIndex` to `timestamp` for plotting
+        x = df.index.to_timestamp()
+        bar_width = 10 # width of bars in bar graphs
 
-        n, x = len(df), np.arange(n)
+        axes[0,0].bar(x, df.distance_sum.apply(m_to_mile), bar_width,
+                      label='total distance (%s)' %'miles')
+        self.plot_fill(x, df.distance_max.apply(m_to_mile), 
+            df.distance_mean.apply(m_to_mile),
+                  ax=axes[0,1], label='distance (%s)' %'miles')
 
-        ax = axes[0,0]           
-        df.distance_sum.apply(m_to_mile).plot(kind='bar', ax=ax,
-                                              label='distance (%s)' %'mile')
-        ax = axes[0,1]
-        ax.plot(x, df.distance_mean.apply(m_to_mile), 'bo', label='avg distance (%s)' %'miles')
-        ax.plot(x, df.distance_max.apply(m_to_mile),  'ro', label='max distance (%s)' %'miles')
-        ax.locator_params(nbins=n+1, axis='x')
-        ax.set_xticklabels(df.index, rotation=90)
-        l = ax.legend(shadow=True)
-        frame = l.get_frame()
-        frame.set_facecolor('0.0')
+        # moving time - total, average, max
+        axes[1,0].bar(x, df.moving_time_sum.apply(ns_to_hms), bar_width,
+                      label='total moving_time (%s)' %'hrs')
+        self.plot_fill(x, df.moving_time_max.apply(ns_to_hms), 
+            df.moving_time_mean.apply(ns_to_hms),
+            ax=axes[1,1], label='moving_time (%s)' %'hrs')
 
-        ax = axes[1,0]
-        df.moving_time_sum.apply(ns_to_hr).plot(kind='bar', ax=ax, 
-                                          label='moving_time (%s)' %'hrs')
-        ax = axes[1,1]
-        ax.plot(x, df.moving_time_mean.apply(ns_to_hr), 'bo', label='avg time (%s)' %'hrs')
-        ax.plot(x, df.moving_time_max.apply(ns_to_hr),  'ro', label='max time (%s)' %'hrs')
-        ax.locator_params(nbins=n+1, axis='x')
-        ax.set_xticklabels(df.index, rotation=90)
-        ax.legend()
-
-        # heartrate
+        # average speed
         ax = axes[2,0]
-        ax.plot(x, df.average_heartrate_mean, 'bo', label='< avg_heartrate >')
-        ax.plot(x, df.average_heartrate_max,  'ro', label='max avg_heartrate')
-        ax.locator_params(nbins=n+1, axis='x')
-        ax.set_xticklabels(df.index, rotation=90)
-        ax.set_ylim([150,200])
-        ax.legend()
-        
+        self.plot_fill(x, df.average_speed_max.apply(speed_to_pace), 
+                  df.average_speed_mean.apply(speed_to_pace),
+                  ax=ax, label='avg_pace', fill=False)
+        ax.set_ylim(ax.get_ylim()[::-1])
+        ax.set_ylim([11,6])
+
+        # elevation gain
+        self.plot_fill(x, df.total_elevation_gain_max, df.total_elevation_gain_sum,
+                  ax=axes[2,1], label='elevation gain (m)')
+        axes[2,1].set_ylim([0,1000])
+
+        # average heartrate
+        self.plot_fill(x, df.average_heartrate_max, df.average_heartrate_mean,
+                  ax=axes[3,0], label='avg_heartrate', fill=False)
+        axes[3,0].set_ylim([150,200])
+
+        # average cadence
+        self.plot_fill(x, df.average_cadence_mean, df.average_cadence_max,
+                  ax=axes[3,1], label='avg_cadence', fill=False)
+        axes[3,1].set_ylim([75,90])
+
+        for ax in axes.reshape(-1): 
+            # format the xticknames
+            ax.locator_params(nbins=len(df)+1, axis='x')
+            ax.xaxis.set_tick_params(reset=True)
+            ax.xaxis.set_minor_locator(mdates.MonthLocator())
+            ax.xaxis.set_minor_formatter(mdates.DateFormatter('%b'))
+            ax.xaxis.set_major_locator(mdates.YearLocator())
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('\n%Y'))
+
+            ax.legend()
+                
         return
